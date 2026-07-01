571c3004376695c69c56d56bdcead6b05d5a7531
Laerte Giraldelli <laerte.giraldelli@maistodos.com.br>
2026-07-01 12:37:47 -0300

    terraform eks v0

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -4,15 +4,12 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  cluster_name    = "meu-cluster-existente"
-  cluster_version = "1.31"
+  cluster_name    = var.name-cluster
+  cluster_version = var.cluster-version
 
   # 1. Configuração de Rede Básica
-  vpc_id     = "vpc-XXXXXXXXXXXXX" # Insira o ID da sua VPC existente
-  subnet_ids = [
-    "subnet-publica-1",          # Insira suas subnets públicas aqui
-    "subnet-publica-2"
-  ]
+  vpc_id     = var.vpc-id # Insira o ID da sua VPC existente
+  subnet_ids = var.subnet-pub
 
   # Endpoint subnets públicas e privadas
   cluster_endpoint_public_access  = true
@@ -20,34 +17,28 @@ module "eks" {
 
   # Security Groups
   create_cluster_security_group = false
-  cluster_security_group_id     = "sg-cluster-existente-XXXXX" # Seu SG do Cluster
+  cluster_security_group_id     = var.sg-clsuter
 
   create_node_security_group    = false
-  node_security_group_id        = "sg-nodes-existente-XXXXX"   # Seu SG dos Workers
+  node_security_group_id        = sg-node
 
   # Grupo de Nós
   eks_managed_node_groups = {
     workers_privados = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size     = var.ng-min
+      max_size     = var.ng-max
+      desired_size = var.ng-desired
 
       # rodar as instâncias nas subnets privadas
-      subnet_ids = [
-        "subnet-privada-1",
-        "subnet-privada-2"
-      ]
+      subnet_ids = var.subnet-priv
 
-      instance_types = ["t3.medium"]
+      instance_types = var.node-types
       
       # Garante que as instâncias herdem o SG correto dos nós
       attach_cluster_primary_security_group = false
     }
   }
 
-  tags = {
-    Environment = "prod"
-    ManagedBy   = "Terraform"
-  }
+  tags = var.tags
 }
 
--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -11,58 +11,64 @@ variable "name-cluster" {
   default     = ""
 }
 
+variable "version-cluster" {
+  description = "Nome EKS cluster"
+  type        = string
+  default     = "1.31"
+}
+
 variable "vpc-id" {
   description = "VPC EKS cluster"
   type        = string
-  default     = ""
+  default     = "vpc-0b6653f3c399f280e"
 }
 
 variable "subnet-pub" {
   description = "Subnets publicas EKS cluster"
   type        = string
-  default     = ""
+  default     = ["subnet-094f6170166da27fb", "subnet-0c8e53aad5c7b625d"]
 }
 
 variable "subnet-priv" {
   description = "Subnets privadas EKS cluster"
   type        = string
-  default     = ""
+  default     = ["subnet-08d54292acc4ec4b0", "subnet-0ac57f9475edcd3fe"]
 }
 
 variable "sg-cluster" {
   description = "SG  EKS cluster"
   type        = string
-  default     = ""
+  default     = "sg-0e9c7d3b2dc3a083a"
 }
 
 variable "sg-node" {
   description = "SG nodes EKS cluster"
   type        = string
-  default     = ""
+  default     = "sg-0e9c7d3b2dc3a083a"
 }
 
 variable "ng-min" {
   description = "min nodes EKS cluster"
-  type        = string
-  default     = "1"
+  type        = integer
+  default     = 1
 }
 
 variable "ng-max" {
   description = "max nodes EKS cluster"
-  type        = string
-  default     = "2"
+  type        = integer
+  default     = 2
 }
 
 variable "ng-desired" {
   description = "desired nodes EKS cluster"
-  type        = string
-  default     = "2"
+  type        = integer
+  default     = 2
 }
 
 variable "node-type" {
   description = "Instance Type EKS cluster"
   type        = string
-  default     = "2"
+  default     = ["t2.medium"]
 }
 
 variable "tags" {
